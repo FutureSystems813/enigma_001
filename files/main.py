4a0f76d1fa08f2e40938ecf16eee26813bfcdb65
FutureSystems813 <futuresystems1323@gmail.com>
2020-01-29 17:09:35 +0100

    try to implement the file creation

--- files/main.py
+++ files/main.py
@@ -1,5 +1,20 @@
 from files.read_input import *
 
-prom_text = read_input.text_to_array()
+file_content = read_input.text_to_array()
 
-print(prom_text)
+def generate_csv():
+    with open("C:/enigma/codec_file.csv", "w+") as coded_file:
+        for line in file_content:
+            coded_file.write(line)
+
+generate_csv()
+
+
+# import os
+# path = "C:/enigma"
+# try:
+#   os.mkdir(path)
+# except OSError:
+#   print("creation failed %s " % path)
+# else
+#   print("succes %s " % path)
