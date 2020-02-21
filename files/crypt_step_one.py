6ea2f6a1f664809ac0d7497c2ae1044cd750d635
Markus Weinzettl <weinzettl0904@gmail.com>
2020-02-21 10:37:23 +0100

    div changes

--- files/crypt_step_one.py
+++ files/crypt_step_one.py
@@ -1,8 +1,14 @@
 from files.read_input import read_input
 
-# store input content in new array
+i = 0
 
-array_to_crypt_one = read_input
+# store input content in new string
+
+array_to_crypt_one = str(read_input)
+
+while i < len(array_to_crypt_one):
+    if array_to_crypt_one[i] in crypt_one_base:
+
 
 class Crypt_Step_One:
 
--- files/main.py
+++ files/main.py
@@ -2,4 +2,5 @@ from files.read_input import read_input
 
 prom_text = read_input.text_to_array()
 
-print(prom_text)
+
+
