b6a42f44a8ca04799933020dcf040b768b23736c
Markus Weinzettl <weinzettl0904@gmail.com>
2020-02-21 11:16:02 +0100

    div changes

--- files/crypt_one_base.py
+++ files/crypt_one_base.py
@@ -31,6 +31,6 @@ class Crypt_One_Base:
     y = round((26-25)**(math.sqrt(432)))
     z = round((26-26)**(math.sqrt(432)))
 
-    crypt_one_base_dict.update([("a", a), ("b", b), ("c", c), ("d", d), ("e", e), ("f", f), ("g", g), ("h", h), ("i", i)
-                                , ("j", j), ("k", k), ("l", l),("m", m), ("n", n), ("o", o), ("p", p), ("q", q), ("r", r)
-                                , ("s", s), ("t", t), ("u", u), ("v", v), ("w", w), ("x", x), ("y", y), ("z", z)])
+    crypt_one_base_dict.update([("a", a), ("b", b), ("c", c), ("d", d), ("e", e), ("f", f), ("g", g), ("h", h), ("i", i),
+                                ("j", j), ("k", k), ("l", l), ("m", m), ("n", n), ("o", o), ("p", p), ("q", q), ("r", r),
+                                ("s", s), ("t", t), ("u", u), ("v", v), ("w", w), ("x", x), ("y", y), ("z", z)])
